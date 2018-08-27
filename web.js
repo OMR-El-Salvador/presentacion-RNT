c5ff982676c89c25bc35833c0cb735313e7d0897
Gual Escalante <gual23@gmail.com>
2018-08-27 15:48:55 -0600

    Added favicon and removed logger dependency

--- web.js
+++ web.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 5000;
-var logger = require('morgan');
 
-app.use(logger);
 app.use('/', express.static(__dirname));
 app.listen(port, function() {
   console.log('Server started on ' + port);

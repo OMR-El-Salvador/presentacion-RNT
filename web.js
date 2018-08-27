b09ea90a0bf37beb0b9fea0ebc8fbd93ee74b859
Gual Escalante <gual23@gmail.com>
2018-08-27 15:41:00 -0600

    Fixed logger express dependency

--- web.js
+++ web.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 5000;
+var logger = require('morgan');
 
-app.use(express.logger());
+app.use(logger);
 app.use('/', express.static(__dirname));
 app.listen(port, function() {
   console.log('Server started on ' + port);
